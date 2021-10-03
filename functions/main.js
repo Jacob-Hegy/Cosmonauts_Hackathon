33ece80503b3121c3236d703feb9469f2e8af1fe
Vanessa Zambrano <vanlouisezam@gmail.com>
2021-10-02 23:50:15 -0500

    firebase partially injected

---
+++ functions/main.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDbusyMan78yGyLNtyM8paGKgG9LtASqW4",
+    authDomain: "horizontal-data-327923.firebaseapp.com",
+    projectId: "horizontal-data-327923",
+    storageBucket: "horizontal-data-327923.appspot.com",
+    messagingSenderId: "16493237544",
+    appId: "1:16493237544:web:00162e15c6048cdc0cec1d",
+    measurementId: "G-FNCQDSR1YJ"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  //const firestore = firebase = firestore(); //<- this was in tutorial but don't know if it's necessary
+
+  //const createForm = document.querySelector("#createForm") //<--this syntax helps with brevity for firebase thingz
+
+  
+
